0834eb1bc48905a4914106f5a67a243dc45dbed9
gabrielccarvalho <caampos22@gmail.com>
2019-09-08 14:28:46 -0300

    chore: add a cpf field

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -7,6 +7,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        cpf: Sequelize.STRING,
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
