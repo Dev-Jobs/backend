a156b91b53ea41509ee9d9acb8df030e0857b6a1
gabrielccarvalho <caampos22@gmail.com>
2019-09-10 19:15:55 -0300

    feat: add dotenv to the project

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+import 'dotenv/config';
+
 import express from 'express';
+import Youch from 'youch';
 import routes from './routes';
 
 import './database';
@@ -17,6 +20,16 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
--- src/database/index.js
+++ src/database/index.js
@@ -24,13 +24,10 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect(
-      'mongodb+srv://devjobs:admin@notification-cf1xp.mongodb.net/test?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useFindAndModify: true,
-      }
-    );
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
   }
 }
 
