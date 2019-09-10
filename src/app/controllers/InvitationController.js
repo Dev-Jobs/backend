cfce676f878cb7113e7bc0f3866d0e1bed8206c5
gabrielccarvalho <caampos22@gmail.com>
2019-09-10 16:20:57 -0300

    feat: add verification system to invitations

--- src/app/controllers/InvitationController.js
+++ src/app/controllers/InvitationController.js
@@ -5,6 +5,16 @@ class InvitationController {
   async index(req, res) {
     const { page = 1 } = req.query;
 
+    const checkIsCompany = await User.findOne({
+      where: { id: req.userId, company: true },
+    });
+
+    if (!checkIsCompany) {
+      return res
+        .status(401)
+        .json({ error: 'You need to be a company to have invitations' });
+    }
+
     const invitations = await Invitation.findAll({
       where: { company_id: req.userId },
       order: ['created_at'],
@@ -20,11 +30,37 @@ class InvitationController {
       ],
     });
 
+    if (!invitations) {
+      return res.status(404).json({
+        error: 'Oops... It seems that you do not have any invitations',
+      });
+    }
+
     return res.json(invitations);
   }
 
   async store(req, res) {
+    const checkIsCompany = await User.findOne({
+      where: { id: req.userId, company: true },
+    });
+
+    if (!checkIsCompany) {
+      return res
+        .status(401)
+        .json({ error: 'You need to be a company to send invitations' });
+    }
+
     const { user_id } = req.body;
+
+    const checkIsUser = await User.findOne({
+      where: { id: user_id, company: false },
+    });
+
+    if (!checkIsUser) {
+      return res
+        .status(401)
+        .json({ error: 'You can not send invitations to another company' });
+    }
 
     const invitation = await Invitation.create({
       user_id,
