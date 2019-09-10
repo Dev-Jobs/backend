574947dc9bd7df066198c5e8e0309da2dc9e90bd
gabrielccarvalho <caampos22@gmail.com>
2019-09-10 16:27:13 -0300

    feat: add verification system to applications

--- src/app/controllers/ApplicationController.js
+++ src/app/controllers/ApplicationController.js
@@ -5,6 +5,16 @@ class ApplicationController {
   async index(req, res) {
     const { page = 1 } = req.query;
 
+    const checkIsUser = await User.findOne({
+      where: { id: req.userId, company: false },
+    });
+
+    if (!checkIsUser) {
+      return res
+        .status(401)
+        .json({ error: 'You can not have applications being a company' });
+    }
+
     const applications = await Application.findAll({
       where: { user_id: req.userId },
       order: ['created_at'],
@@ -20,11 +30,37 @@ class ApplicationController {
       ],
     });
 
+    if (!applications) {
+      return res.status(404).json({
+        error: 'Oops... It seems that you do not have any application',
+      });
+    }
+
     return res.json(applications);
   }
 
   async store(req, res) {
+    const checkIsUser = await User.findOne({
+      where: { id: req.userId, company: false },
+    });
+
+    if (!checkIsUser) {
+      return res
+        .status(401)
+        .json({ error: 'You can not send applications being a company' });
+    }
+
     const { company_id } = req.body;
+
+    const checkIsCompany = await User.findOne({
+      where: { id: company_id, company: true },
+    });
+
+    if (!checkIsCompany) {
+      return res
+        .status(401)
+        .json({ error: 'You can not send invitations to another user' });
+    }
 
     const application = await Application.create({
       company_id,
