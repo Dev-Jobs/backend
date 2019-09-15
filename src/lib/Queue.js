f66fdca7bef2583afeac2959065b0811e228fcad
gabrielccarvalho <caampos22@gmail.com>
2019-09-15 15:26:43 -0300

    fix: fix queue error

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -30,8 +30,6 @@ class Queue {
   processQueue() {
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
-
-      bee.on('failed', this.handleFailure).process(handle);
     });
   }
 }
